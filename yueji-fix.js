b7fe27aa08a45f7a9a4bb6f242dca125aa743968
MERCYY177 <xiaoshu10088@outlook.com>
2026-09-07 20:28:35 +0800

    Refresh layout bundle for report features

--- yueji-fix.js
+++ yueji-fix.js
@@ -2,7 +2,7 @@
   'use strict';
   const load=(src,done)=>{
     const s=document.createElement('script');
-    s.src=src+'?v=20260907-covers-green-overflow';
+    s.src=src+'?v=20260907-report-export-delete-colors';
     if(done)s.onload=done;
     document.head.appendChild(s);
   };
