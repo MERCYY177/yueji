302049065c823a794c96deac51b530bf047948dc
MERCYY177 <xiaoshu10088@outlook.com>
2026-09-07 22:20:34 +0800

    Load book cover editor and full-year calendar fix

--- yueji-fix.js
+++ yueji-fix.js
@@ -1,10 +1,10 @@
 (()=>{
   'use strict';
-  const load=(src,done,version='20260907-weread-evidence2')=>{
+  const load=(src,done)=>{
     const s=document.createElement('script');
-    s.src=src+'?v='+version;
+    s.src=src+'?v=20260907-cover-calendar';
     if(done)s.onload=done;
     document.head.appendChild(s);
   };
-  load('yueji-weread-evidence.js',()=>load('yueji-fix-core.js',()=>load('yueji-layout.js')));
+  load('yueji-weread-evidence.js',()=>load('yueji-fix-core.js',()=>load('yueji-layout.js',()=>load('yueji-book-cover.js'))));
 })();
